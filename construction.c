1e48c39df3766913cfaa2fd1cb1b45328a785cef
djiang2 <djiang2>
2018-10-02 11:10:55 -0400

    Trying struct

--- construction.c
+++ construction.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int main(){
+  givename(first);
+}
+
+struct foo{char *name; int x;};
+struct foo first;
+struct foo p;
+
+p = &first;
+
+int givename(struct foo a){
+  printf("Struct value: %s[%d}",p->name,p->x);
+  return 0;
+}
